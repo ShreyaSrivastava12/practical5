97381122fc68de63884106d61dbd201b4a17385c
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2021-09-30 10:17:59 +0530

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+void displayc()
+{
+      char a='A';
+      cout<<a<<endl;
+}
+void displayi()
+{
+      int a=76098;
+      cout<<a<<endl;
+}
+void displays()
+{
+      short a=45;
+      cout<<a<<endl;
+}
+void displayl()
+{
+      long a=86989436;
+      cout<<a<<endl;
+}
+void displayf()
+{
+      float a=98.3f;
+      cout<<a<<endl;
+}
+void displayd()
+{
+      double a=7869;
+      cout<<a<<endl;
+}
+void displayb()
+{
+      bool a=(22>12);
+      cout<<a<<endl;
+}
+void displayld()
+{
+      long double a=78877L;
+      cout<<a<<endl;
+}
+int main()
+{   
+      displayc();
+      displayi();
+      displays();
+      displayl();
+      displayf();
+      displayd();
+      displayb();
+      displayld();
+}
